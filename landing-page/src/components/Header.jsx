2ecfaaa1adbde86cbf28d43bdef515e3db222ed9
devanaclimgo <anaclimgo@gmail.com>
2025-03-17 19:26:53 -0300

    header arquivo feito

---
+++ landing-page/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { logo } from '../assets'
+
+const Header = () => {
+  const navList = [
+    { id: 'sobre', text: 'Sobre' },
+    { id: 'funcionalidades', text: 'Funcionalidades' },
+    { id: 'equipe', text: 'Equipe' },
+  ]
+
+  return (
+    <header>
+      <nav>
+        <img src={logo} alt="Logo InsuCheck" width={186} height={68} />
+        <div className='listaHeader'>
+          {navList.map((nav) => {
+            <a key={nav.id} href={`#${nav.id}`}>
+            <div>
+              {nav.text}
+            </div>
+          </a>
+          })}
+        </div>
+
+        <div className='downloadBtn'>
+          <button>Download</button>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
