db83ec1c4256c0b7976cbad7fb5671d301bf560e
devanaclimgo <anaclimgo@gmail.com>
2025-03-18 21:00:43 -0300

    rewriting code for better rendering

--- landing-page/src/components/Header.jsx
+++ landing-page/src/components/Header.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import logo from '../assets/logo.svg'
+import React from 'react';
+import logo from '../assets/logo.svg';
 
 const Header = () => {
   const navList = [
     { id: 'sobre', text: 'Sobre' },
     { id: 'funcionalidades', text: 'Funcionalidades' },
     { id: 'equipe', text: 'Equipe' },
-  ]
+  ];
 
   return (
     <header>
       <nav>
         <img src={logo} alt="Logo InsuCheck" width={186} height={68} />
-        <div className='listaHeader'>
+        <div className="listaHeader">
           {navList.map((nav) => {
-            <a key={nav.id} href={`#${nav.id}`}>
-            <div>
-              {nav.text}
-            </div>
-          </a>
+            return (
+              <a key={nav.id} href={`#${nav.id}`}>
+                <div>{nav.text}</div>
+              </a>
+            );
           })}
         </div>
 
-        <div className='downloadBtn'>
+        <div className="downloadBtn">
           <button>Download</button>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
