86e4e1a78188a5eddf7fffa42d2f60421e8ba070
devanaclimgo <anaclimgo@gmail.com>
2025-03-24 18:31:55 -0300

    footer componente adicionado

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx';
 import Banner from './components/Banner.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div style={{ margin: 0 }}>
       <Header />
       <Banner />
+      <Footer />
     </div>
   );
 }
