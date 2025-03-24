eab0854d710b3733eef26ef5a587a103866ca5a0
devanaclimgo <anaclimgo@gmail.com>
2025-03-23 22:02:06 -0300

    centralizando conteúdo

--- landing-page/src/assets/styles/Header.styles.js
+++ landing-page/src/assets/styles/Header.styles.js
@@ -6,14 +6,13 @@ export const GlobalStyle = styled.div`
 `;
 
 export const Container = styled.div`
-  font-family: font/family/Body;
   max-width: 1500px;
   margin: 0 auto;
   padding: 10px 20px;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
   position: fixed;
   left: 0;
   right: 0;
@@ -21,6 +20,8 @@ export const Container = styled.div`
   background: rgba(255, 255, 255, 0.8);
   backdrop-filter: blur(6px);
   border-bottom: 1px solid rgba(77, 77, 77, 0.2);
+  padding-right: 158px;
+  padding-left: 158px;
 `;
 
 export const Nav = styled.div`
@@ -34,7 +35,7 @@ export const ListaHeader = styled.div`
   display: flex;
   align-items: center;
   gap: 15px;
-  font-size: 16px;
+  font-size: 13px;
 
   a {
     color: #333;
@@ -52,11 +53,14 @@ export const Button = styled.button`
   color: #2f39d3;
   font-weight: 700;
   line-height: 24px;
-  font-size: 18px;
-  padding: 10px 20px;
+  font-size: 14px;
   border: 1.5px solid #2f39d3;
   border-radius: 8px;
   cursor: pointer;
+  padding-top: 10px;
+  padding-right: 22px;
+  padding-bottom: 10px;
+  padding-left: 22px;
 
   &:hover {
     background-color: #2f39d3;
