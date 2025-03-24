71e6e2d1f1ae543341c5042e86c74163cf720daf
devanaclimgo <anaclimgo@gmail.com>
2025-03-24 19:05:18 -0300

    retirado fonte global

--- landing-page/src/main.jsx
+++ landing-page/src/main.jsx
@@ -1,4 +1,3 @@
-import { UrbanistFont } from './assets/styles/Header.styles'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,8 +6,6 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UrbanistFont>
       <App />
-    </UrbanistFont>
   </React.StrictMode>,
 );
