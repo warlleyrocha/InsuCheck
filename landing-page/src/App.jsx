d9ff061f33ca2129d93984157d3ad6e7d82fcc5e
devanaclimgo <anaclimgo@gmail.com>
2025-03-23 21:30:49 -0300

    adicionando banner component

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header.jsx'
+import Banner from './components/Banner.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   )
 }
