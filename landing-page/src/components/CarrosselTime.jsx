afd906c29307f6dcdc2abe5aaa58eb0ee6f30f31
devanaclimgo <anaclimgo@gmail.com>
2025-03-28 17:36:58 -0300

    adicionado container e text

--- landing-page/src/components/CarrosselTime.jsx
+++ landing-page/src/components/CarrosselTime.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Fundo } from '../assets/styles/CarrosselTime.styles'
+import { Container, Fundo, Text } from '../assets/styles/CarrosselTime.styles'
 
 
 const CarrosselTime = () => {
   return (
     <Fundo>
-      
+      <Container>
+        <Text>
+          <h1>Nosso Time</h1>
+          <p>Conheça as mentes criativas por trás do nosso produto. Nossa equipe é formada por especialistas dedicados a entregar a melhor experiência para você!</p>
+        </Text>
+      </Container>
     </Fundo>
   )
 }
