d3b4219b80a0dee1c525af10e5355ffa90e06698
Ana <anaclimgo@gmail.com>
2025-04-06 16:58:13 -0300

    imagem iphone adicionada

--- landing-page/src/components/Intro.jsx
+++ landing-page/src/components/Intro.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Text } from '../assets/styles/Intro.styles'
 import { Button } from '../assets/styles/Banner.styles'
+import IphoneImg from '../assets/images/1IPhone13Pro.svg'
 
 const Intro = () => {
   return (
@@ -10,6 +11,7 @@ const Intro = () => {
         <p>Experimente as funcionalidades em primeira mão e nos dê seu feedback!</p>
         <Button>Teste o APK</Button>
       </Text>
+      <img src={IphoneImg} width={275} height={595} />
     </Container>
   )
 }
