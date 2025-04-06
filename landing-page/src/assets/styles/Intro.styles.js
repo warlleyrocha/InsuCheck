6e77cc93c1aa0c79382e936535c3e6d60a55df07
Ana <anaclimgo@gmail.com>
2025-04-06 17:07:11 -0300

    espaçamento ajustado intro

--- landing-page/src/assets/styles/Intro.styles.js
+++ landing-page/src/assets/styles/Intro.styles.js
@@ -2,37 +2,42 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin: 0 auto;
-  padding-top: 120px;
-  margin-bottom: 120px;
+  max-width: 1512px;
+  padding-top: 90px;
+  padding-bottom: 200px;
   font-family: 'Urbanist', sans-serif;
   margin: 0 auto;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   width: 100%;
+
+  img {
+    padding-right: 260px;
+  }
 `;
 
 export const Text = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  align-items: left;
+  padding-left: 60px;
 
   h1 {
     color: #1E272F;
     font-size: 44px;
     font-weight: 700;
-    line-height: 18.4px;
+    line-height: 60px;
+    width: 20ch;
   }
 
   p {
     color: #1E272FCC;
     font-size: 20px;
     font-weight: 500;
-    line-height: 29.8px;
+    line-height: 20px;
     flex-wrap: wrap;
     width: 80ch;
-    text-align: center;
   }
 `;
