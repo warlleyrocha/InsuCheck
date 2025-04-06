58caa2ef68387f5600f63da4bbb9ac654481bc78
Ana <anaclimgo@gmail.com>
2025-04-06 18:17:31 -0300

    imagem colocada dentro de uma div

--- landing-page/src/components/Intro.jsx
+++ landing-page/src/components/Intro.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { Container, Text } from '../assets/styles/Intro.styles'
-import { Button } from '../assets/styles/Banner.styles'
-import IphoneImg from '../assets/images/1IPhone13Pro.svg'
+import React from 'react';
+import { Container, Text } from '../assets/styles/Intro.styles';
+import { Button } from '../assets/styles/Banner.styles';
+import IphoneImg from '../assets/images/1IPhone13Pro.svg';
 
 const Intro = () => {
   return (
     <Container>
       <Text>
         <h1>Teste Nosso Aplicativo Agora!</h1>
-        <p>Experimente as funcionalidades em primeira mão e nos dê seu feedback!</p>
+        <p>
+          Experimente as funcionalidades em primeira mão e nos dê seu feedback!
+        </p>
         <Button>Teste o APK</Button>
       </Text>
-      <img src={IphoneImg} width={275} height={595} />
+      <div className='img-container'>
+        <img src={IphoneImg} width={275} height={595} />
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
