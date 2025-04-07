2dfdeecc08d3ee259be457d6fc185e0d1f89828c
Ana <anaclimgo@gmail.com>
2025-04-07 11:25:05 -0300

    responsivo adicionado

--- landing-page/src/assets/styles/Banner.styles.js
+++ landing-page/src/assets/styles/Banner.styles.js
@@ -10,9 +10,23 @@ export const Container = styled.div`
   gap: 20px;
   background: rgba(253, 253, 253, 1);
   width: 100%;
+  height: auto;
+  overflow-x: auto;
+  scroll-behavior: smooth;
 
   img {
     padding-top: 150px;
+  }
+
+  @media (max-width: 995px) {
+    flex-direction: column-reverse;
+    padding-bottom: 80px;
+
+    img {
+      width: 440px;
+      height: 440px;
+      padding-top: 170px;
+    }
   }
 `;
 
@@ -37,6 +51,15 @@ export const ContainerTexto = styled.div`
     line-height: 30px;
     color: rgba(51, 51, 51, 1);
     font-family: 'Lato', sans-serif;
+  }
+
+  @media (max-width: 995px) {
+    width: 70%;
+    padding-top: 60px;
+
+    p {
+      width: 50ch
+    }
   }
 `;
 
